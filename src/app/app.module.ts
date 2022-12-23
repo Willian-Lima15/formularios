6276921b593f6f4e4a8326e698ef94118754247b
Willian Lima <william_slayer@hotmail.com>
2022-12-23 11:52:11 -0300

    Criado componente de mensagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { MensagemComponent } from './components/mensagem/mensagem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-      FormularioComponent
+      FormularioComponent,
+      MensagemComponent
    ],
   imports: [
     BrowserModule,
